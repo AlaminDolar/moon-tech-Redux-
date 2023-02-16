474e80e4984d5152944a7d7ab5698b37175d7d81
alamindolar <amin15-9898@diu.edu.bd>
2023-02-16 15:16:45 +0600

    axios configure

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -2,21 +2,25 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
 import { toggle, togglebrands } from "../../features/filter/filterSlice";
+import { getProducts } from "../../features/products/productsSlice";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
   const dispatch = useDispatch();
   const filter = useSelector((state) => state.filter);
+  const { products, isLoading } = useSelector((state) => state.products);
   const { brands, stock } = filter;
+
+  console.log(brands, stock);
   useEffect(() => {
-    fetch("products.json")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
+    dispatch(getProducts());
   }, []);
 
   const activeClass = "text-white  bg-indigo-500 border-white";
 
   let content;
+  if (isLoading) {
+    content = <h1>Loading..... </h1>;
+  }
 
   if (products.length) {
     content = products.map((product) => (
