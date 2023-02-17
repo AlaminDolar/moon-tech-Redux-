1017d8f5afd8808168c184e5cd482f0342f6c08a
Al-AminDolar <alamin.dolar1@gmail.com>
2023-02-17 15:28:30 +0600

    async thunk finished here

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,10 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchProducts, postProduct } from "./productsAPI";
+import { deleteProduct, fetchProducts, postProduct } from "./productsAPI";
 
 const initialState = {
   products: [],
   isLoading: false,
   postSuccess: false,
+  deleteSuccess: false,
   isError: false,
   error: "",
 };
@@ -24,9 +25,30 @@ export const addProduct = createAsyncThunk(
     return products;
   }
 );
+export const removeProduct = createAsyncThunk(
+  "products/removeProduct",
+  async (id, thunkAPI) => {
+    const products = deleteProduct(id);
+    thunkAPI.dispatch(removeFromList(id));
+    return products;
+  }
+);
 const productsSlice = createSlice({
   name: "products",
   initialState,
+  reducers: {
+    togglePostSuccess: (state) => {
+      state.postSuccess = false;
+    },
+    toggleDeleteSuccess: (state) => {
+      state.deleteSuccess = false;
+    },
+    removeFromList: (state, action) => {
+      state.products = state.products.filter(
+        (product) => product._id !== action.payload
+      );
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(getProducts.pending, (state, action) => {
@@ -58,8 +80,26 @@ const productsSlice = createSlice({
         state.isError = true;
         state.postSuccess = false;
         state.error = action.error.message;
+      })
+      .addCase(removeProduct.pending, (state) => {
+        state.isLoading = true;
+        state.deleteSuccess = false;
+        state.isError = false;
+      })
+      .addCase(removeProduct.fulfilled, (state) => {
+        state.isLoading = false;
+        state.deleteSuccess = true;
+      })
+      .addCase(removeProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.products = [];
+        state.isError = true;
+        state.deleteSuccess = false;
+        state.error = action.error.message;
       });
   },
 });
 
+export const { toggleDeleteSuccess, togglePostSuccess, removeFromList } =
+  productsSlice.actions;
 export default productsSlice.reducer;
